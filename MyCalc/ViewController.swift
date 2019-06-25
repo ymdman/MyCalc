901cb7ec3e0f1bdc787795f429025eedb14225de
ymdman <revitalized@icloud.com>
2019-06-25 23:01:55 +0900

    Initial Commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by kazuhiroyamada on 2019/06/25.
+//  Copyright © 2019 kazuhiroyamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
